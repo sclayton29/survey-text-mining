9452a3b2c4c1ce8b6111b043b24835739ddfb548
Sarah Clayton <sarah.c.clayton@gmail.com>
2017-08-10 15:30:09 -0500

    Added custom stopwords

--- test-survey.R
+++ test-survey.R
@@ -27,13 +27,15 @@ corpus <- tm_map(corpus, removePunctuation)
 corpus <- tm_map(corpus, content_transformer(tolower))
 #Removing English stopwords. Might prefer to custom list for project, removes all version of not -- could be problematic
 corpus_nostopwords <-corpus #saving a version of the corpus without stopwords for later use
-corpus <- tm_map(corpus, removeWords, stopwords("english"))
+myStopwords <- c(stopwords('english'), "college") #Add other words you want to exclude to this list
+corpus <- tm_map(corpus, removeWords, myStopwords)
 #striping extra whitespace
 corpus <- tm_map(corpus, stripWhitespace)
 #stemming words -- may not want to do - I left it out because it didn't seem helpful. Uncomment the next line to try it out.
 #corpus <- tm_map(corpus, stemDocument)
 #checking cleaned data
-inspect(corpus[30])
+inspect(corpus_nostopwords[32])
+inspect(corpus[32])
 
 
 #Creating a Term Document Matrix
@@ -48,6 +50,8 @@ findFreqTerms(dtm, lowfreq=50) #only returns words that appear 50 times or more
 findAssocs(dtm, "job", 0.2)
 findAssocs(dtm, "good", 0.2)
 findAssocs(dtm, "wanted", 0.2)
+findAssocs(dtm, "high", 0.5)
+findAssocs(dtm, "school", 0.2)
 
 #Trying to plot
 #putting data in a plottable format
@@ -64,12 +68,13 @@ p
 
 #Creating a wordcloud
 #limit words by specifying minimum frequency
-wordcloud(names(freqr), freqr, min.freq = 2, colors=brewer.pal(6, "Dark2"))
+wordcloud(names(freqr), freqr, min.freq = 40, colors=brewer.pal(6, "Dark2"))
 
 #Trying out clustering
 #Removing sparse terms
 dtm_rm_sparse <- removeSparseTerms(dtm, 0.98) #removes terms that only appear in 2% of the responses -- have to do this or cluster will be unreadable
 dtm_rm_sparse
+
 
 #Hierarchal Clustering
 dtm_rm_sparse
@@ -82,8 +87,8 @@ plot(fit, hang=-1)
 #identifing clusters
 plot.new()
 plot(fit, hang=-1)
-groups <- cutree(fit, k=5) #k is the number of cluster
-rect.hclust(fit, k=5, border="red")
+groups <- cutree(fit, k=10) #k is the number of cluster
+rect.hclust(fit, k=10, border="red")
 
 # Doing some sentiment analysis
 #turning it into tidy data (one document per row data frame)
@@ -121,7 +126,7 @@ rowTotals <- apply(dtm , 1, sum) #Find the sum of words in each Document
 dtm.new   <- dtm[rowTotals> 0, ]           #remove all docs without words
 
 #set a seed so that the output of model is predictable
-corpus_lda <-LDA(dtm.new, k=2, control = list(seed =1399)) #Need to figure out exactly what seed parameter does
+corpus_lda <-LDA(dtm.new, k=2, control = list(seed =150)) 
 
 corpus_topics <-tidy(corpus_lda, matrix="beta")
 corpus_topics
@@ -159,26 +164,14 @@ tidy(dtm.new) %>%
   arrange(desc(count))
 
 #Using some of the terms to pull assign cateogories to entries. Using regex patterns with keywords that might apply
-family = "parents|family|mother|father"
+family = "parent|family|mother|father"
 career = "career|job"
-knowledge = "knowledge|learning"
-money = "money|salary|pay|financ|income"
+knowledge = "knowledge|learn|education"
+money = "money|salary|pay|financ|income|debt"
+location = "distance|location|home|close"
 
 #Writing a loop to crawl through the assigned terms
 inspect(corpus_nostopwords)
-inspect(corpus_nostopwords[[5]])
-
-corpus_nostopwords[[5]]
-
-response <- corpus_nostopwords[[5]]
-string <- "education "
-bool <- grepl(family, response)
-if (bool == TRUE) {
-  print("ok")
-}
-
-
-
 
 #Checking family words
 total_family = 0
@@ -231,3 +224,18 @@ for (doc in 1:corpus_length){
   }
 }
 print(total_money)
+
+
+#Checking for location
+total_location = 0
+for (doc in 1:corpus_length){
+  keywords = location
+  response <- corpus_nostopwords[[doc]]
+  status <-grepl(keywords, response)
+  if (status == TRUE){
+    total_location = total_location +1
+    print(paste0(doc, ": ", corpus_nostopwords[[doc]]))
+  }
+}
+print(total_location)
+
